5127280de824ea8af435757f70b99fe0a763514a
Michał Szewczak <michasacuer3@gmail.com>
2019-01-06 17:15:24 +0100

    Database init

---
+++ Farm_ASPCoreEFReactApp_Backend/Farm_ASPCoreEFReactApp/Models/DbContext/FarmDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Farm_ASPCoreEFReactApp.Models
+{
+    public class FarmDbContext : DbContext
+    {
+        public FarmDbContext(DbContextOptions<FarmDbContext> options) : base(options) { }
+
+        public DbSet<Farm> Farms { get; set; }
+    }
+}
